9609d78bef3dab3e966b4dfe657b318b33ac8e0b
Omar Razi <startfresh87@gmail.com>
2022-01-16 14:38:49 -0500

    commit tree cda942b61e0a973d35dd2636e3f0986b257ac298

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+function createFareMultiplier(integer) {
+  return function (fare) {
+    return fare * integer;
+  };
+}
+
+const fareDoubler = function (fare) {
+  return fare * 2;
+};
+const fareTripler = function (fare) {
+  return fare * 3;
+};
+const selectDifferentDrivers = function (drivers, f) {
+  if (f === returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers(drivers);
+  } else if (f === returnLastTwoDrivers) {
+    return returnLastTwoDrivers(drivers);
+  }
+};
